f80397bfca32af51bd1ef16c81328d8fbcd901ad
tolik518 <function@returnnull.de>
2025-02-27 23:42:26 +0100

    - added refresh and get_user route - added feature-toggle for unnecessary routes - moved api to dedicated file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use reqwest::{Client, Url};
-use serde_json::json;
-use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
+use reqwest::Url;
 use std::env;
 use dotenv::dotenv;
 use serde::Deserialize;
+use crate::strong_api::StrongApi;
+
+mod strong_api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -12,78 +13,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let password = env::var("STRONG_PASS").expect("STRONG_PASS must be set");
     let strong_backend = env::var("STRONG_BACKEND").expect("STRONG_BACKEND must be set");
 
-    let url = Url::parse(format!("{strong_backend}/auth/login").as_str()).ok().unwrap();
+    let url = Url::parse(&*strong_backend).ok().unwrap();
 
     let mut strong_api = StrongApi::new(url);
 
     strong_api.login(username.as_str(), password.as_str()).await?;
+    strong_api.get_user().await?;
 
     dbg!(strong_api.access_token);
     dbg!(strong_api.refresh_token);
     dbg!(strong_api.user_id);
 
     Ok(())
-}
-
-pub struct StrongApi {
-    url: Url,
-    headers: HeaderMap,
-    client: Client,
-    refresh_token: Option<String>,
-    access_token: Option<String>,
-    user_id: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-struct LoginResponse {
-    #[serde(rename = "accessToken")]
-    access_token: Option<String>,
-    #[serde(rename = "refreshToken")]
-    refresh_token: Option<String>,
-    #[serde(rename = "userId")]
-    user_id: Option<String>,
-}
-
-impl StrongApi {
-    pub fn new(url: Url) -> Self {
-        let mut headers = HeaderMap::new();
-        headers.insert(HeaderName::from_static("user-agent"), HeaderValue::from_static("Strong Android"));
-        headers.insert(HeaderName::from_static("content-type"), HeaderValue::from_static("application/json"));
-        headers.insert(HeaderName::from_static("accept"), HeaderValue::from_static("application/json"));
-        headers.insert(HeaderName::from_static("x-client-build"), HeaderValue::from_static("600013"));
-        headers.insert(HeaderName::from_static("x-client-platform"), HeaderValue::from_static("android"));
-
-        Self {
-            url,
-            headers,
-            client: Client::new(),
-            refresh_token: None,
-            access_token: None,
-            user_id: None,
-        }
-    }
-
-    pub async fn login(&mut self, username: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let body = json!({
-            "usernameOrEmail": username,
-            "password": password
-        });
-
-        let response = self.client
-            .post(self.url.clone())
-            .headers(self.headers.clone())
-            .json(&body)
-            .send()
-            .await?;
-
-        let response_text = response.text().await?;
-
-        let parsed: LoginResponse = serde_json::from_str(&response_text)?;
-
-        self.access_token = parsed.access_token;
-        self.refresh_token = parsed.refresh_token;
-        self.user_id = parsed.user_id;
-
-        Ok(())
-    }
 }
---
+++ src/strong_api.rs
@@ -0,0 +1,155 @@
+use std::fmt::format;
+use reqwest::{Client, header::{HeaderMap, HeaderName, HeaderValue}, Url};
+use serde::Deserialize;
+use serde_json::json;
+
+#[derive(Debug)]
+pub struct StrongApi {
+    url: Url,
+    headers: HeaderMap,
+    client: Client,
+    pub(crate) refresh_token: Option<String>,
+    pub(crate) access_token: Option<String>,
+    pub(crate) user_id: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginResponse {
+    #[serde(rename = "accessToken")]
+    access_token: Option<String>,
+    #[serde(rename = "refreshToken")]
+    refresh_token: Option<String>,
+    #[serde(rename = "userId")]
+    user_id: Option<String>,
+}
+
+impl StrongApi {
+
+    /// Creates a new StrongApi instance with the provided backend URL.
+    pub fn new(url: Url) -> Self {
+        let mut headers = HeaderMap::new();
+        headers.insert(HeaderName::from_static("user-agent"), HeaderValue::from_static("Strong Android"));
+        headers.insert(HeaderName::from_static("content-type"), HeaderValue::from_static("application/json"));
+        headers.insert(HeaderName::from_static("accept"), HeaderValue::from_static("application/json"));
+        headers.insert(HeaderName::from_static("x-client-build"), HeaderValue::from_static("600013"));
+        headers.insert(HeaderName::from_static("x-client-platform"), HeaderValue::from_static("android"));
+
+        Self {
+            url,
+            headers,
+            client: Client::new(),
+            refresh_token: None,
+            access_token: None,
+            user_id: None,
+        }
+    }
+
+    /// Logs in to the Strong backend using the provided username and password.
+    pub async fn login(&mut self, username: &str, password: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.url.join("auth/login").unwrap();
+
+        let body = json!({
+            "usernameOrEmail": username,
+            "password": password
+        });
+
+        let response = self.client
+            .post(url)
+            .headers(self.headers.clone())
+            .json(&body)
+            .send()
+            .await?;
+
+        let response_text = response.text().await?;
+
+        let parsed: LoginResponse = serde_json::from_str(&response_text)?;
+
+        self.access_token = parsed.access_token;
+        self.refresh_token = parsed.refresh_token;
+        self.user_id = parsed.user_id;
+
+        Ok(())
+    }
+
+    /// Refreshes the access token using the access and refresh token which were obtained during login.
+    /// Should be called when you receive a 401 Unauthorized response from the Strong backend.
+    pub async fn refresh(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.url.join("auth/login/refresh").unwrap();
+
+        let body = json!({
+            "accessToken": self.access_token,
+            "refreshToken": self.refresh_token
+        });
+
+        let response = self.client
+            .post(url)
+            .bearer_auth(self.access_token.clone().unwrap())
+            .headers(self.headers.clone())
+            .json(&body)
+            .send()
+            .await?;
+
+        dbg!(response.status());
+
+        let response_text = response.text().await?;
+
+        let parsed: LoginResponse = serde_json::from_str(&response_text)?;
+
+        self.access_token = parsed.access_token;
+        self.refresh_token = parsed.refresh_token;
+
+        Ok(())
+    }
+
+    #[cfg(feature = "full")]
+    pub async fn refresh_by_tokens(
+        &mut self, access_token: String,
+        refresh_token: String
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.url.join("auth/login/refresh").unwrap();
+
+        let body = json!({
+            "accessToken": access_token.clone(),
+            "refreshToken": refresh_token,
+        });
+
+        let response = self.client
+            .post(url)
+            .bearer_auth(access_token)
+            .headers(self.headers.clone())
+            .json(&body)
+            .send()
+            .await?;
+
+        dbg!(response.status());
+
+        let response_text = response.text().await?;
+
+        let parsed: LoginResponse = serde_json::from_str(&response_text)?;
+
+        self.access_token = parsed.access_token;
+        self.refresh_token = parsed.refresh_token;
+
+        Ok(())
+    }
+
+    pub async fn get_user(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let user_id = &*self.user_id.clone().unwrap();
+        let url = self.url.join(format!("api/users/{user_id}").as_str()).unwrap();
+
+        let response = self.client
+            .get(url)
+            .bearer_auth(self.access_token.clone().unwrap())
+            .headers(self.headers.clone())
+            .send()
+            .await?;
+
+        dbg!(response.status());
+
+        let response_text = response.text().await?;
+
+        dbg!(response_text);
+
+        Ok(())
+    }
+}
